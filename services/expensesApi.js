685dcdf20f1316dc124cfaec842dc8d3f5d2da74
Julio <julio.ramirez@auth0.com>
2021-07-09 13:26:19 +0200

    Update expensesApi.js

--- services/expensesApi.js
+++ services/expensesApi.js
@@ -27,7 +27,7 @@ const expensesApi = {
        },
       };
       const res = await fetch(`${window.env.API_URL}/reports`, options);
-      const json = await res.json(); 
+      const json = await res.json();
       return json;
       // 👆 Replace this with call to API 👆
     } catch (err) {
